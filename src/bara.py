608c1dd34f90a8d301d0b323dee8d10a5ac9a17d
Baru0 <89037916+Baru0@users.noreply.github.com>
2022-05-17 21:22:20 +0200

    Update bara.py

---
+++ src/bara.py
@@ -0,0 +1,22 @@
+from sklearn.svm import SVC
+
+
+class MLClassificator(SVC):
+    def __init__(self):
+        super().__init__()
+
+    @staticmethod
+    def transform(X):
+        return X.reshape(X.shape[0], X.shape[1] * X.shape[2])
+
+    def fit(self, X, y):
+        X = self.transform(X)
+        super().fit(X, y.argmax(axis=-1))
+        return self
+
+    def predict(self, X):
+        return super().predict(self.transform(X))
+
+    @property
+    def name(self):
+        return 'SVM'
