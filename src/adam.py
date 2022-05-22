2873b4250700850156959d5e8c5c60c1bc82a6e9
azit99 <adam.zitnansky@gmail.com>
2022-05-22 06:46:46 +0000

    added TSAI LSTM classificator

--- src/adam.py
+++ src/adam.py
@@ -1,7 +1,9 @@
 from sklearn.naive_bayes import GaussianNB
 from tensorflow import keras as K
+from tsai.all import *
+import numpy as np
 
-
+#machine learning classificator
 class MLClassificator(GaussianNB):
     def __init__(self):
         super().__init__(priors=None, var_smoothing=1e-09)
@@ -21,3 +23,38 @@ class MLClassificator(GaussianNB):
     @property
     def name(self):
         return 'GaussianNB'
+
+#deep learning calssificator
+
+class TSAI:
+    def __init__(self, arch):
+        self.arch = arch
+
+    def fit(self, X_train, y_train, **args):
+        X_val, y_val = args['validation_data']
+        X, y, splits = combine_split_data([X_train, X_val], [y_train, y_val])
+        y = OneHot(n_classes=y.shape[1]).decode(y)
+        self.model = TSClassifier(
+            X,
+            y,
+            splits=splits,
+            bs=[64, 128],
+            batch_tfms=[TSStandardize()],
+            arch=self.arch,
+            metrics=accuracy
+        )
+        self.model.fit_one_cycle(25, lr_max=1e-3)
+
+    def predict(self, X):
+        y = np.array(self.model.get_X_preds(X)[2], dtype='int')
+        return y
+
+    @property
+    def name(self):
+        return str(self.arch).split('.')[-1][:-2]
+
+
+class LSTMClassificator(TSAI):
+    def __init__(self):
+        super().__init__(LSTM)
+    
